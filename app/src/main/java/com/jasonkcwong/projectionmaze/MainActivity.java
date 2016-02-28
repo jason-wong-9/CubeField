2e2670c5053f771e969c64c721bea42c507435e4
Jason Wong <jason_19960903@hotmail.com>
2016-02-27 22:50:18 -0800

    Getting average of rotation every 1/25 seconds

--- app/src/main/java/com/jasonkcwong/projectionmaze/MainActivity.java
+++ app/src/main/java/com/jasonkcwong/projectionmaze/MainActivity.java
@@ -6,10 +6,11 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
-    
+
     Sensor rotationvector;
     SensorManager sm;
 
@@ -44,34 +45,53 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     float azimut;
     float pitch;
     float roll;
+    float azimutSum = 0;
+    float pitchSum = 0;
+    float rollSum = 0;
+    float azimutCount = 0;
+    float pitchCount = 0;
+    float rollCount = 0;
 
     private long lastUpdate = 0;
 
     @Override
     public void onSensorChanged(SensorEvent event) {
 
-/*        Log.i("TAG", (System.currentTimeMillis() - lastUpdate) + "");
-        if (System.currentTimeMillis() - lastUpdate < 1000) {
-            return;
-        }*/
+        Log.i("TAG", (System.currentTimeMillis() - lastUpdate) + "");
+        if (System.currentTimeMillis() - lastUpdate < 400) {
 
-        lastUpdate = System.currentTimeMillis();
+            float orientation[] = new float[3];
 
+            if( event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR ){
+                float R[] = new float[9];
+                // calculate th rotation matrix
+                SensorManager.getRotationMatrixFromVector(R, event.values);
+                // get the azimuth value (orientation[0]) in degree
 
-        float orientation[] = new float[3];
-
-        if( event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR ){
-            float R[] = new float[9];
-            // calculate th rotation matrix
-            SensorManager.getRotationMatrixFromVector(R, event.values);
-            // get the azimuth value (orientation[0]) in degree
-
-            azimut = SensorManager.getOrientation(R, orientation)[0];
-            pitch = SensorManager.getOrientation(R, orientation)[1];
-            roll = SensorManager.getOrientation(R, orientation)[2];
+                azimut = SensorManager.getOrientation(R, orientation)[0];
+                azimutSum += azimut;
+                azimutCount++;
+                pitch = SensorManager.getOrientation(R, orientation)[1];
+                pitchSum += pitch;
+                pitchCount++;
+                roll = SensorManager.getOrientation(R, orientation)[2];
+                rollSum += roll;
+                rollCount++;
+            }
+        } else {
+            lastUpdate = System.currentTimeMillis();
+            float azimutAvg = azimutSum/azimutCount;
+            float pitchAvg = pitchSum/pitchCount;
+            float rollAvg = rollSum/rollCount;
+            String s = String.format("Z: %.3f%nX: %.3f%nY: %.3f", Math.toDegrees(azimutAvg), Math.toDegrees(pitchAvg), Math.toDegrees(rollAvg));
+            displayReading.setText(s);
+            azimutSum = 0;
+            azimutCount = 0;
+            pitchSum = 0;
+            pitchCount = 0;
+            rollSum = 0;
+            rollCount = 0;
         }
-        String s = String.format("Z: %.3f%nX: %.3f%nY: %.3f", Math.toDegrees(azimut), Math.toDegrees(pitch), Math.toDegrees(roll));
-        displayReading.setText(s);
     }
 synchronized
     @Override
